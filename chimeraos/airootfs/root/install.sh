5d182dc2f67af9c34de5f48e1222acc482a88046
honjow <honjow311@gmail.com>
2023-12-03 15:46:50 +0800

    add boot entries

--- chimeraos/airootfs/root/install.sh
+++ chimeraos/airootfs/root/install.sh
@@ -23,9 +23,9 @@ fi
 sleep 2
 
 TARGET="stable"
-while ! ( curl -Ls https://github.com | grep '<html' > /dev/null ); do
+while ! ( curl -Ls https://baidu.com | grep '<html' > /dev/null ); do
     whiptail \
-     "未检测到互联网连接。请使用网络配置工具激活网络，然后选择 <Quit> 以退出工具并继续安装。" \
+     "未检测到互联网连接。请使用网络配置工具激活网络，然后选择 <退出> 以退出工具并继续安装。" \
      12 50 \
      --yesno \
      --yes-button "网络配置" \
@@ -35,13 +35,13 @@ while ! ( curl -Ls https://github.com | grep '<html' > /dev/null ); do
          exit 1
     fi
 
-    nmtui-connect
+    LANG=zh_CN.UFT-8 nmtui-connect
 done
 #######################################
 
 MOUNT_PATH=/tmp/frzr_root
 
-if ! frzr-bootstrap gamer; then
+if ! LANG=zh_CN.UFT-8 frzr-bootstrap gamer; then
     whiptail --msgbox "系统引导步骤失败\n输入 ./install.sh 可以重新开始" 10 50
     exit 1
 fi
@@ -57,7 +57,7 @@ fi
 
 # Grab the steam bootstrap for first boot
 
-URL="https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steam-jupiter-stable-1.0.0.76-1-x86_64.pkg.tar.zst"
+URL="https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steam-jupiter-stable-1.0.0.78-1.2-x86_64.pkg.tar.zst"
 TMP_PKG="/tmp/package.pkg.tar.zst"
 TMP_FILE="/tmp/bootstraplinux_ubuntu12_32.tar.xz"
 DESTINATION="/tmp/frzr_root/etc/first-boot/"
@@ -83,7 +83,10 @@ fallback_keyword="使用备用源"
 if [ "$MENU_SELECT" = "Advanced Install" ]; then
   OPTIONS=$(whiptail --separate-output --checklist "Choose options" 10 55 4 \
     "$firmware_overrides_keyword" "DSDT/EDID" OFF \
-    "$unstable_keyword" "" OFF 3>&1 1>&2 2>&3)
+    "$unstable_keyword" "" OFF \
+    "$cdn_keyword" "" OFF \
+    "$fallback_keyword" "" ON \
+    3>&1 1>&2 2>&3)
 
   if echo "$OPTIONS" | grep -q "$firmware_overrides_keyword"; then
     echo "启用固件覆盖..."
@@ -112,10 +115,15 @@ EOL
   if echo "$OPTIONS" | grep -q "$cdn_keyword"; then
     sed -i "s/^release_cdn.*/release_cdn = true/" /etc/frzr-sk.conf
     sed -i "s/^api_cdn.*/api_cdn = true/" /etc/frzr-sk.conf
+  else
+    sed -i "s/^release_cdn.*/release_cdn = false/" /etc/frzr-sk.conf
+    sed -i "s/^api_cdn.*/api_cdn = false/" /etc/frzr-sk.conf
   fi
 
   if echo "$OPTIONS" | grep -q "$fallback_keyword"; then
     sed -i "s/^fallback_url.*/fallback_url = true/" /etc/frzr-sk.conf
+  else
+    sed -i "s/^fallback_url.*/fallback_url = false/" /etc/frzr-sk.conf
   fi
 
 fi
